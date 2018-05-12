58aaeb9cf0cfc2b624589b95065d16ebbbc91337
Aris Lacdao <aceraven777@gmail.com>
2018-05-12 18:59:17 +0800

    Rename libraries folder

--- src/PayMayaServiceProvider.php
+++ src/PayMayaServiceProvider.php
@@ -14,7 +14,7 @@ class PayMayaServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../library/User.php' => app_path('Libraries/PayMaya/User.php'),
+            __DIR__.'/../libraries/User.php' => app_path('Libraries/PayMaya/User.php'),
         ]);
     }
 
